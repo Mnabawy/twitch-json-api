bebed168e55b2dd6cdf23b1810be2fff90f7e998
Mnabawy <m_ninja61@yahoo.com>
2021-09-16 12:47:08 +0200

    handled the bqckend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/styles';
+import { useDemoData } from '@mui/x-data-grid-generator';
+import { DataGrid } from '@mui/x-data-grid';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      channels: [
+        "ESL_SC2",
+        "OgamingSC2",
+        "cretetion",
+        "freecodecamp",
+        "storbeck",
+        "habathcx",
+        "RobotCaleb",
+        "noobs2ninjas",
+      ],
+      channelsDetails: [],
+    };
+  }
+
+  componentDidMount() {
+    this.getChannelDetails();
+    this.getStreamDetails();
+  }
+
+  async getStreamDetails() {
+    const url = "https://api.twitch.tv/helix/streams/";
+    const config = {
+      headers: {
+        "Authorization": "Bearer w3d9qo30x7yiyob3kq4vtu1xovuaxs",
+        "Client-Id": "8r3cnqtyyefyq0lhhethv2tpwzoe0m",
+      },
+      params: {
+        user_login: this.state.channels.map(channel => channel),
+      },
+    };
+    await axios
+      .get(url, config)
+      .then(response => {
+        let res = response.data.data
+        let channels = this.state.channelsDetails
+
+        const test = channels.forEach((channel) => {
+          let arr = res.map((stream) => {
+            if (channel.id === stream.user_id) {
+              channel['status'] = 'online';
+              channel['title'] = stream.title;
+              channel['game_name'] = stream.game_name
+            }
+          })
+        })
+
+        this.setState({ channelsDetails: channels });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  getChannelDetails() {
+    const url = "https://api.twitch.tv/helix/users/";
+    const config = {
+      headers: {
+        Authorization: "Bearer w3d9qo30x7yiyob3kq4vtu1xovuaxs",
+        "Client-Id": "8r3cnqtyyefyq0lhhethv2tpwzoe0m",
+      },
+      params: {
+        login: this.state.channels.map(channel => channel),
+      },
+    };
+    axios
+      .get(url, config)
+      .then(response => {
+        const list = [...response.data.data];
+        this.setState({ channelsDetails: list })
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+  render() {
+    console.log('channel details', this.state.channelsDetails)
+
+    const { channelsDetails: channels } = this.state
+    return (
+      < div >
+        <h1>TWITCH STREAMERS</h1>
+        {
+          channels.map(channel => (
+            <div key={channel.id}>
+              <li><img src={channel.profile_image_url}></img></li>
+              <li>{channel.display_name}</li>
+              {channel.status == "online"
+                ? <p>{`${channel.game_name}  ${channel.title}`}</p>
+                : 'Offline'
+              }
+            </div>
+          ))
+        }
+        <ul>
+        </ul>
+      </div >
+    )
+  }
 }
+
 
 export default App;
---
+++ test.js
@@ -0,0 +1,140 @@
+let channelDetails = [
+    {
+        broadcaster_type: "partner",
+        created_at: "2014-09-24T15:06:58Z",
+        description: "",
+        display_name: "OgamingSC2",
+        id: "71852806",
+        login: "ogamingsc2",
+        offline_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/,bdc4be6e-fe18-477b-adc8-758015fdc628-channel_offline_image-1920x1080.png",
+        profile_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/,c874a498-6fc9-4004-b543-a2feafa8862b-profile_image-300x300.png",
+        type: "",
+        view_count: 75584823,
+    },
+    {
+        broadcaster_type: "affiliate",
+        created_at: "2015-05-06T15:57:39Z",
+        description: "I am a retired Police Officer.  I believe in the Thin Blue Line.  I support Law ,Enforcement, Firefighters, and EMS.  I am a Sheep Dog and I am part of a Pack.  I want you to join my ,Pack.  We can fight the Evil in the world together.",
+        display_name: "cretetion",
+        id: "90401618",
+        login: "cretetion",
+        offline_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/,cretetion-channel_offline_image-0410bb4dec3a9991-1920x1080.jpeg",
+        profile_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/,cretetion-profile_image-12bae34d9765f222-300x300.jpeg",
+        type: "",
+        view_count: 99577
+    },
+    {
+        broadcaster_type: "",
+        created_at: "2015-01-14T03:36:47Z",
+        description: "Learn to code for free at freecodecamp.org",
+        display_name: "FreeCodeCamp",
+        id: "79776140",
+        login: "freecodecamp",
+        offline_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/freecodecamp-channel_offline_image-b8e133c78cd51cb0-1920x1080.png",
+        profile_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/,freecodecamp-profile_image-d9514f2df0962329-300x300.png",
+        type: "",
+        view_count: 223119,
+    }, {
+        broadcaster_type: "",
+        created_at: "2017-04-05T20:20:03Z",
+        description: "",
+        display_name: "Storbeck",
+        id: "152475255",
+        login: "storbeck",
+        offline_image_url: "",
+        profile_image_url: "https://static-cdn.jtvnw.net/user-default-pictures-uv/ead5c8b2-a4c9-4724-b1dd-9f00b46cbd3d-profile_image-300x300.png",
+        type: "",
+        view_count: 2190,
+    }, {
+        broadcaster_type: "",
+        created_at: "2009-06-07T15:04:54Z",
+        description: "League of Legends, Music, Nerd Stuff, Chat.. blah blah blah",
+        display_name: "habathcx",
+        id: "6726509",
+        login: "habathcx",
+        offline_image_url: "",
+        profile_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/habathcx-profile_image-d75385dbe4f42a66-300x300.jpeg",
+        type: "",
+        view_count: 2939,
+
+    }, {
+        broadcaster_type: "",
+        created_at: "2014-01-13T04:07:33Z",
+        description: "",
+        display_name: "RobotCaleb",
+        id: "54925078",
+        login: "robotcaleb",
+        offline_image_url: "",
+        profile_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/robotcaleb-profile_image-9422645f2f0f093c-300x300.png",
+        type: "",
+        view_count: 7014,
+    }, {
+        broadcaster_type: "affiliate",
+        created_at: "2015-02-13T08:13:10Z",
+        description: "Live Programming my face off while hanging out with the best viewers Twitch has to offer!",
+        display_name: "noobs2ninjas",
+        id: "82534701",
+        login: "noobs2ninjas",
+        offline_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/noobs2ninjas-channel_offline_image-7f974925e9dc942c-1920x1080.jpeg",
+        profile_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/noobs2ninjas-profile_image-34707f847a73d934-300x300.png",
+        type: "",
+        view_count: 55960,
+    }, {
+        broadcaster_type: "partner",
+        created_at: "2012-05-02T09:59:20Z",
+        description: "For standings, schedule, and results, visit http://www.intelextrememasters.com/",
+        display_name: "ESL_SC2",
+        id: "30220059",
+        login: "esl_sc2",
+        offline_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/ac0e240d-63c2-4faf-9040-60543b3e2c0f-channel_offline_image-1920x1080.jpeg",
+        profile_image_url: "https://static-cdn.jtvnw.net/jtv_user_pictures/cac110f7-0711-421f-9341-c77b53ab63b5-profile_image-300x300.jpeg",
+        type: "",
+        view_count: 102412097,
+    }
+
+]
+
+let streamSetails = [
+    {
+        game_id: "490422",
+        game_name: "StarCraft II",
+        id: "43266489404",
+        is_mature: false,
+        language: "en",
+        started_at: "2021-09-15T03:56:27Z",
+        tag_ids: ["36a89a80-4fcd-4b74-b3d2-2c6fd9b30c95", "6ea6bca4-4712-4ab9-a906-e3336a9d8039"],
+        thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_esl_sc2-{width}x{height}.jpg",
+        title: "RERUN: Maru vs. Reynor- DreamHack SC2 Masters Summer: Season Finals - Grand Final",
+        type: "live",
+        user_id: "30220059",
+        user_login: "esl_sc2",
+        user_name: "ESL_SC2",
+        viewer_count: 509,
+    },
+    {
+        game_id: "490422",
+        game_name: "StarCraft II",
+        id: "43272752508",
+        is_mature: false,
+        language: "fr",
+        started_at: "2021-09-15T21:53:29Z",
+        tag_ids: (3)["6f655045-9989-4ef7-8f85-1edcec42d648", "36a89a80-4fcd-4b74-b3d2-2c6fd9b30c95", "a005b174-b20e-46b1-8679-4589fef81c8e"],
+        thumbnail_url: "https://static-cdn.jtvnw.net/previews-ttv/live_user_ogamingsc2-{width}x{height}.jpg",
+        title: "[REDIFFUSIONS] ESL Open Cup #88 avec : Harstem, MarineLord, Clem, HeroMarine, Maxpax, Zest, ,Nightmare, Spirit, Garitos, Shadown etc",
+        type: "live",
+        user_id: "71852806",
+        user_login: "ogamingsc2",
+        user_name: "OgamingSC2",
+        viewer_count: 62,
+    }
+]
+
+
+
+channelDetails.forEach((channel) => {
+    let arr = streamSetails.map((stream) => {
+        if (channel.id === stream.user_id) {
+            console.log('equal')
+        }
+    })
+})
